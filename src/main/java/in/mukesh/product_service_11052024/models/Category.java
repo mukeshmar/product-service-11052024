f5df4d2ef1a0b803ce4b9e1a75193d6114ed75c8
mukesh_mar <144646641+mukeshmar@users.noreply.github.com>
2024-05-22 11:24:41 +0530

    Add JPA dependency to the project

--- src/main/java/in/mukesh/product_service_11052024/models/Category.java
+++ src/main/java/in/mukesh/product_service_11052024/models/Category.java
@@ -1,11 +1,18 @@
 package in.mukesh.product_service_11052024.models;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
-public class Category {
+@Entity
+public class Category extends BaseModel {
     private String title;
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
--- src/main/java/in/mukesh/product_service_11052024/models/Product.java
+++ src/main/java/in/mukesh/product_service_11052024/models/Product.java
@@ -1,15 +1,19 @@
 package in.mukesh.product_service_11052024.models;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-    private long id;
+@Entity
+public class Product extends BaseModel {
     private String title;
     private String description;
     private Double price;
     private String image;
+    @ManyToOne(cascade = {CascadeType.PERSIST})
     private Category category;
 }
